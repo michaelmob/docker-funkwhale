147babfe6cc22fe2b461fb1c902d4f7c2fc3189b
thetarkus <thetarkus@users.noreply.github.com>
2019-02-03 20:05:48 -0500

    Shebang portability; simplify sed command

--- scripts/download-nginx-template.sh
+++ scripts/download-nginx-template.sh
@@ -1,5 +1,5 @@
-
-#!/bin/bash -eu
+#!/usr/bin/env bash
+set -eu
 
 dest_dir=$1
 artifact_version=$2
@@ -11,12 +11,12 @@ mkdir -p $dest_dir
 dest_file="$dest_dir/funkwhale_nginx.template"
 echo "Downloading $artifact_url to $dest_file…"
 wget "$artifact_url" -O $dest_file
-echo "Fixing file content for use in our docker image…"
-sed -i "s/api:5000/127.0.0.1:8000/" $dest_file
-sed -i "s/listen 80;/listen 80 default_server;/" $dest_file
-sed -i "s/server_name \${FUNKWHALE_HOSTNAME};/server_name _;/" $dest_file
-sed -i "s/\$http_upgrade/\${_}http_upgrade/" $dest_file
-sed -i "s/\$connection_upgrade/\${_}connection_upgrade/" $dest_file
-sed -i "s|/frontend|/app/front/dist|" $dest_file
+echo 'Fixing file content for use in our docker image…'
+sed -i -e 's/api:5000/127.0.0.1:8000/' \
+	-e 's/listen 80;/listen 80 default_server;/' \
+	-e 's/server_name \${FUNKWHALE_HOSTNAME};/server_name _;/' \
+	-e 's/\$http_upgrade/\${_}http_upgrade/' \
+	-e 's/\$connection_upgrade/\${_}connection_upgrade/' \
+	-e 's|/frontend|/app/front/dist|' $dest_file
 
-echo "Done!"
+echo 'Done!'
